fdab4745424c7477e5257a4fc0fff4e14ef664ef
Simon McVittie <smcv@collabora.com>
2022-09-05 09:21:38 +0100

    Add a Meson build system
    Signed-off-by: Simon McVittie <smcv@collabora.com>

---
+++ doc/maintainer-update-website.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Copyright 2022 Collabora Ltd.
+# SPDX-License-Identifier: MIT
+
+me="$(readlink -f "$0")"
+here="$(dirname "$me")"
+top="$(dirname "$here")"
+
+DBUS_TOP_SRCDIR="$top" python3 "$here/redirects.py"
+rsync -rtvzPp --chmod=Dg+s,ug+rwX,o=rX \
+    doc/_build/ \
+    "${DOC_RSYNC_DEST-dbus.freedesktop.org:/srv/dbus.freedesktop.org/www/doc/dbus-python}/"
